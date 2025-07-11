0d03093932c798f7397c20eccee15f03681f5858
Rui Pinto <ruispinto@gmail.com>
2025-07-11 10:28:59 +0100

    Add project files.

---
+++ Converter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Converter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
